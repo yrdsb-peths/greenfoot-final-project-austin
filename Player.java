e71514fd16d30a4dcc700f5d88c0dd8d0055f070
AustinYip <101162675+AustinYip@users.noreply.github.com>
2022-06-03 14:16:58 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -8,19 +8,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Player extends Actor
 {
-    /**
-     * Act - do whatever the ControllablePlayer wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+    //theme song variable
+    GreenfootSound tsong = new GreenfootSound("themesong.mp3");
+    //walking animation variable
+    GreenfootImage[] walking = new GreenfootImage[9];
+    
+    /*
+     * contructor - the code that gets run one time when the object is created
      */
+    public Player()
+    {
+        for (int i = 0; i<9; i++)
+        {
+            walking[i] = new GreenfootImage("images/walking/walking" + i + ".png");
+        }
+        setImage(walking[0]);
+    }
     public void act()
     {
         if(Greenfoot.isKeyDown("left"))
         {
-            move(-2);
+            move(-8);
+            //Background theme song, putting it here so it plays again and again on loop
+            tsong.play();
         }
         if(Greenfoot.isKeyDown("right"))
         {
-            move(2);
+            move(8);
+            //Background theme song
+            tsong.play();
         }
     }
 }
